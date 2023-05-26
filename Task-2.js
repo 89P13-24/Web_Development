3c0efdcc03361323f136afd807445d782b99832d
89P13-24 <134151658+89P13-24@users.noreply.github.com>
2023-05-27 00:17:02 +0530

    Update Task-2.js

--- Task-2.js
+++ Task-2.js
@@ -4,7 +4,7 @@ console.log(2);
 
 const getTodos = async () => {
 
-    const response = await fetch('data.json');
+    const response = await fetch('https://www.coursehubiitg.in/api/codingweek/contributions');
     const data = await response.json();
 
    
@@ -12,9 +12,9 @@ const getTodos = async () => {
     var l = data.length;
    // var arr = data;
     var temp;
-    /*for(var i=0;i<l;i++){
-        for(var j=i;j<l-1;j++){
-            if(data[j+1].points<data[j].points){
+   for(var i=0;i<l;i++){
+        for(var j=0;j<l-i-1;j++){
+            if(data[j+1].points>data[j].points){
                 temp=data[j+1];
                 data[j+1]=data[j];
                 data[j]=temp;
@@ -22,19 +22,19 @@ const getTodos = async () => {
             }
         }
 
-    }*/
-    data.sort().reverse();
+    }
+    
     
 
 
-    document.getElementsByClassName("big1")[0].src = data[0].avatar;
-    document.getElementsByClassName("big2")[0].src = data[1].avatar;
-    document.getElementsByClassName("big3")[0].src = data[2].avatar;
+  //  document.getElementsByClassName("big1")[0].src = data[0].avatar;
+   // document.getElementsByClassName("big2")[0].src = data[1].avatar;
+   // document.getElementsByClassName("big3")[0].src = data[2].avatar;
     document.getElementsByClassName("first")[0].innerHTML = data[0].name+"-"+data[0].points;
     document.getElementsByClassName("second")[0].innerHTML = data[1].name+"-"+data[1].points;
     document.getElementsByClassName("third")[0].innerHTML = data[2].name+"-"+data[2].points;
 
-for(var k=3;k<data.length;k++){
+/*for(var k=3;k<data.length;k++){
 
     var white=document.createElement("div");
     white.setAttribute("class","white");
@@ -42,7 +42,7 @@ for(var k=3;k<data.length;k++){
 
     var rnk=document.createElement("div");
 
-}
+}*/
 
     //console.log(arr);
 
